3995c24e94eeac9d7da81b52369f5b5797d25e6c
Mario Santa Cruz <mariosanta_cruz@hotmail.com>
2025-04-26 15:27:33 +0100

    typo

--- src/components/LeaderboardHeader.tsx
+++ src/components/LeaderboardHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/images/logo_squared.png';
+import logo from '../assets/images/logo.png';
 
 const LeaderboardHeader: React.FC = () => {
   return (
